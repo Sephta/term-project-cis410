b512418d239061a4c0ff2aa6f09001d9835249e8
Seth Tal <Seth.tal255@gmail.com>
2020-04-24 23:58:02 -0700

    Minor changes
    - Small updates to player movement. Player now Lerps between run and walk speeds
    
    - Small changes to the scene variables attatched to player

--- term-project-main/Assets/Scripts/BehaviorExecutor.cs
+++ term-project-main/Assets/Scripts/BehaviorExecutor.cs
@@ -19,15 +19,25 @@ public class BehaviorExecutor : MonoBehaviour
     public float walkSpeed = 0f;
     public float runSpeed = 0f;
     public float jumpHeight = 0f;
-    public float jumpVelocity = 0f;
     public float gravity = 9.8f;
 
     [SerializeField] Vector3 directionVector = Vector3.zero;
     [SerializeField] float currentSpeed = 0f;
+    [SerializeField] float jumpVelocity = 0f;
+    
+    // bellow vars are used to lerp between walk and run speeds
+    float tw = 0f;
+    float tr = 0f;
+    float t_acc = 0.02f;
 
     /* ---------------------------------------------------------------- */
     /*                              Updates                             */
     /* ---------------------------------------------------------------- */
+
+    void Start()
+    {
+        currentSpeed = walkSpeed;
+    }
 
     void Update()
     {
@@ -69,16 +79,43 @@ public class BehaviorExecutor : MonoBehaviour
         
         jumpVelocity = Mathf.Sqrt(2 * gravity * jumpHeight);  // in physics this represents velocity just before impact
 
-        if (behaviorMachine.input.runKey)
+        // If the player is running 
+        if (behaviorMachine.input.runKey) {
+            tw = 0f; // resets the lerp t value for walking
+
+            // transition from run to walk by increments of tr
+            currentSpeed = Mathf.Lerp(currentSpeed, runSpeed, tr);
+
+            // update the transform accordingly
             transform.position += new Vector3(
-                directionVector.x * runSpeed * Time.deltaTime,
+                directionVector.x * currentSpeed * Time.deltaTime,
                 jumpVelocity * Time.deltaTime,
-                directionVector.z * runSpeed * Time.deltaTime);
-        else
+                directionVector.z * currentSpeed * Time.deltaTime);
+            
+            // bound the value of tr so it doesnt grow infinitly while moving
+            if (tr < 1f)
+                tr += t_acc;
+            /* The reason its described as a t acceleration is because each
+            time t is added to the currentSpeed variable it is growing 
+            exponentially by a factor of t_acc */
+        }
+        // else player is walking
+        else {
+            tr = 0f; // resets lerp t value for running
+
+            // transition from run to walk by increments of tw
+            currentSpeed = Mathf.Lerp(currentSpeed, walkSpeed, tw);
+
+            // update the transform accordingly
             transform.position += new Vector3(
-                directionVector.x * walkSpeed * Time.deltaTime,
+                directionVector.x * currentSpeed * Time.deltaTime,
                 jumpVelocity * Time.deltaTime,
-                directionVector.z * walkSpeed * Time.deltaTime);
+                directionVector.z * currentSpeed * Time.deltaTime);
+
+            // bounds value so it doesnt keep growing infinitly
+            if (tw < 1f)
+                tw += t_acc;
+        }
         
         if (behaviorMachine.CheckGround()) {
             behaviorMachine.currentBehavior = BehaviorMachine.PlayerBehavior.idle;
@@ -87,11 +124,29 @@ public class BehaviorExecutor : MonoBehaviour
 
     public void ExecuteMove(bool moveFlag)
     {
+
         // if player is walking
         if (!moveFlag) {
-            transform.position += directionVector * walkSpeed * Time.deltaTime;
-        } else {
-            transform.position += directionVector * runSpeed * Time.deltaTime;
+            tr = 0f;
+
+            currentSpeed = Mathf.Lerp(currentSpeed, walkSpeed, tw);
+
+            transform.position += directionVector * currentSpeed * Time.deltaTime;
+
+            if (tw < 1f)
+                tw += t_acc;
+            
+        }
+        // else player is running
+        else {
+            tw = 0f;
+
+            currentSpeed = Mathf.Lerp(currentSpeed, runSpeed, tr);
+            
+            transform.position += directionVector * currentSpeed * Time.deltaTime;
+
+            if (tr < 1f)
+                tr += t_acc;
         }
     }
 }
