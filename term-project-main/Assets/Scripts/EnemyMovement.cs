21f68196cf5220eaeb0b1a735785f0561dc7c9ef
ZackJ94 <63127604+ZackJ94@users.noreply.github.com>
2020-04-25 16:48:40 -0700

    Enemy Movement Prototyped
    1. Built rudimentary navmesh system for enemy movement.
    
    2. Tidied up scene heirarchy.

---
+++ term-project-main/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.AI;
+
+
+public class EnemyMovement : MonoBehaviour
+{
+    public float patrolTime = 8f;
+    public float aggroRange = 5f;
+    public Transform[] waypoints;
+
+    int index;
+    float speed, agentSpeed;
+    Transform player;
+
+    // Animator anim;
+    NavMeshAgent agent;
+
+    void Awake()
+    {
+        // anim = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+
+        if (agent != null) { agentSpeed = agent.speed; }
+
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        index = Random.Range(0, waypoints.Length);
+
+        InvokeRepeating("Tick", 0, 0.5f);
+
+        if (waypoints.Length > 0)
+        {
+            InvokeRepeating("Patrol", 0, patrolTime);
+        }
+    }
+
+    void Patrol()
+    {
+        // if index is at last waypoint, go back to waypoints[0], else iterate index
+        index = index == (waypoints.Length - 1) ? 0 : index + 1;
+    }
+
+
+    void Tick()
+    {
+        // move to next waypoint position
+        agent.destination = waypoints[index].position;
+
+        // set non-aggro speed to "walking pace"
+        // NOTE: could set patSpeed, chaseSpeed, etc as variables and swap between them based on state
+        agent.speed = agentSpeed / 2;
+
+        // if player in aggro range...
+        if (player != null && Vector3.Distance(transform.position, player.position) < aggroRange)
+        {
+            // move to player position at full speed
+            agent.destination = player.position;
+            agent.speed = agentSpeed;
+        }
+    }
+
+}
+
