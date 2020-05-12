649362de6734655362ab86003c3534fab4b99454
BrennanHolmes <brennan.holmes@uoregon.edu>
2020-05-12 11:53:37 -0700

    updated grassland scene

---
+++ term-project-main/Assets/Scripts/LadderController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LadderController : MonoBehaviour
+{
+    void OnTriggerStay(Collider other) {
+        if (other.gameObject.tag == "Player") {
+            other.transform.Translate(Vector3.up * 0.125f);
+        }
+    }
+}
